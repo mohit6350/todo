b94b9efdbdb8a79a01f632a38a4a42cc7bf607e0
mohit6350 <khare.mohit635@gmail.com>
2024-09-15 19:25:06 +0530

    added name change button

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Mohit from "./components/Mohit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Tasker
+      <Mohit name="mohit" age={29 + 1}/>
     </div>
   );
 }
