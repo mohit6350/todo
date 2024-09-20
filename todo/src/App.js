b5cc1b9ca6b31d6bccbe25858de61fe31eb76652
mohit6350 <khare.mohit635@gmail.com>
2024-09-20 18:07:00 +0530

    version 1 completed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,41 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 
 function App() {
- 
-  const [isCompleteScreen, SetIsCompleteScreen] = useState(false)
+  const [isCompleteScreen, SetIsCompleteScreen] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setAllTodos(updatedTodoArr);
+    localStorage.setItem('todo-list',JSON.stringify(updatedTodoArr))
+  };
+
+  useEffect(()=>{
+    let savedTodo = JSON.parse(localStorage.getItem('todo-list'))
+    if(savedTodo){
+      setAllTodos(savedTodo)
+    }
+  },[])
+
+  const deleteTodoItem = (index) => {
+    let oldTodoArr = [...allTodos]
+    oldTodoArr.splice(index,1)
+
+    localStorage.setItem('todo-list',JSON.stringify(oldTodoArr))
+    setAllTodos(oldTodoArr)
+
+  }
 
   return (
     <>
@@ -14,31 +46,66 @@ function App() {
           <div className="todo-input">
             <div className="todo-input-item">
               <label>Title</label>
-              <input type="text" placeholder="What's your new task..."></input>
+              <input
+                type="text"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+                placeholder="What's your new task..."
+              ></input>
             </div>
             <div className="todo-input-item">
-             <label>Description</label>
-              <input type="text" placeholder="Description"></input>
-          </div>
-          <div className="todo-input-item">
-          <button type="button" className="primaryBtn">Add</button>
-          </div>
+              <label>Description</label>
+              <input
+                type="text"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                placeholder="Description"
+              ></input>
+            </div>
+            <div className="todo-input-item">
+              <button
+                type="button"
+                onClick={handleAddTodo}
+                className="primaryBtn"
+              >
+                Add
+              </button>
+            </div>
           </div>
 
-          
           <div className="btn-area">
-              <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`}
+            <button
+              className={`secondaryBtn ${
+                isCompleteScreen === false && "active"
+              }`}
               onClick={() => SetIsCompleteScreen(false)}
-              >todo</button>
-              <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`}
+            >
+              todo
+            </button>
+            <button
+              className={`secondaryBtn ${
+                isCompleteScreen === true && "active"
+              }`}
               onClick={() => SetIsCompleteScreen(true)}
-              >complete</button>            
+            >
+              complete
+            </button>
           </div>
           <div className="todo-list">
-            <div className="todo-list-item">
-              <h3>task 1</h3>
-              <p>Description</p>
-            </div>
+            {allTodos.map((item, index) => {
+              return (
+                <div className="todo-list-item" key={index}>
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                  </div>
+                  <div>
+                    <AiOutlineDelete className="icon" onClick={()=>deleteTodoItem(index)}/>
+                    <BsCheckLg className="check-icon" />
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
